9fa84f4650ede15f123206541d77931eae150788
Malik Vance <malik@atlassian.com>
2024-06-25 19:19:26 -0500

    fixed my array c because i had a space in my title for the file

--- arrays.c
+++ arrays.c
@@ -4,11 +4,11 @@ int main()
 {
 	// array = data structure that can store many values of the same data type.
 
-	double prices[5] = {5.0 10.0, 15.0, 20.0};
+	double prices[5] = {5.0, 10.0, 15.0, 20.0};
 
 	for(int i = 0; i < 5; i++)
 	{
-		printf("lf\n", price [i]);)
+		printf("$%lf\n", prices[i]);
 	}
 	return 0;
 }
